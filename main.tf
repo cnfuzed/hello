d42f1904d819443d2c4318a352ec3ea8f2fc039d
root <root@ip-10-0-28-77.us-east-2.compute.internal>
2023-05-16 19:41:58 +0000

    Fuled

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"  // Amazon Linux 2 AMI ID
+  ami           = "ami-0c94855ba95c71c99"  # Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
 
   tags = {
-    Name = "MyEC
+    Name = "MyEC2Instance"
+  }
+}
 
