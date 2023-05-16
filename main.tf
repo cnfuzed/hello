515d2fffbac0b81ac03259790ac5ab8c6ccf8652
root <root@ip-10-0-28-77.us-east-2.compute.internal>
2023-05-16 21:14:43 +0000

    dafgidsf

--- main.tf
+++ main.tf
@@ -1,8 +1,43 @@
-resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
+provider "aws" {
+  region     = "us-east-1"
+  
+  
+}
 
- tags = {
-   Name = var.name_tag,
- }
+#Create a new EC2 launch configuration
+resource "aws_instance" "ec2_public" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
+  #user_data                   = "${data.template_file.provision.rendered}"
+  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "EC2-PUBLIC"
+  }
+  
+ 
+}
+#Create a new EC2 launch configuration
+resource "aws_instance" "ec2_private" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
+  subnet_id                   = aws_subnet.private-subnet-1.id
+  associate_public_ip_address = false
+  #user_data                   = "${data.template_file.provision.rendered}"
+  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "EC2-PRIVATE"
+  }
+ 
 }
