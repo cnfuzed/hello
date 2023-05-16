03f5dc5f25c611f26365ad015de0920a4dca80ea
root <root@ip-10-0-28-77.us-east-2.compute.internal>
2023-05-16 20:48:09 +0000

    dsdwdwerwe

--- provider.tf
+++ provider.tf
@@ -1,7 +1,11 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
-   }
- }
+  required_providers {
+    aws = {
+      source = "registry.terraform.io/hashicorp/aws"
+      version = "4.14.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+}
