e9526f4510681f5b4946e774ae2b8fe3a41dd091
root <root@ip-10-0-28-77.us-east-2.compute.internal>
2023-05-16 20:37:00 +0000

    bhut time lag gaya

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_access_key" {
   type        = string
-  description = "AWS access key"
+  description = "AKIAT4X4SARV322NEOH7"
 }
 
 variable "aws_secret_key" {
   type        = string
-  description = "AWS secret key"
+  description = "2/RjbBmnaZy/ZGVbE7Bulnq5YgfXct4WYFVVU17R"
 }
 
