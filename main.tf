75b3daaba6fb79d8deb70617d2def74ba2b17ab5
root <root@ip-10-0-28-77.us-east-2.compute.internal>
2023-05-16 20:18:36 +0000

    Initial

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "MyEC2Instance" {
-  ami           = "ami-0c94855ba95c71c99"  // Amazon Linux 2 AMI ID
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
-
   tags = {
-    Name = "MyEC2Instance"
+    Name = "example-instance"
   }
 }
+
